9d27d9f5ecd978ccc2fc559f3fac8d0bd929159e
MortyMaroon <denistkhnv@gmail.com>
2019-11-25 18:57:36 +0300

    Добавлено поле здоровья главного героя.

--- WorldRenderer.java
+++ WorldRenderer.java
@@ -2,24 +2,38 @@ package com.star.app.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.StringBuilder;
+import com.star.app.screen.utils.Assets;
 
 public class WorldRenderer {
     private GameController gc;
     private SpriteBatch batch;
+    private BitmapFont font32;
+    private StringBuilder strBuilder;
 
     public WorldRenderer(GameController gc, SpriteBatch batch) {
         this.gc = gc;
         this.batch = batch;
+        this.font32 = Assets.getInstance().getAssetManager().get("fonts/font32.ttf", BitmapFont.class);
+        this.strBuilder = new StringBuilder();
     }
 
-    public void render(){
+    public void render() {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         gc.getBackground().render(batch);
-        gc.getAsteroid().render(batch);
         gc.getHero().render(batch);
+        gc.getAsteroidController().render(batch);
+        gc.getBulletController().render(batch);
+        strBuilder.clear();
+        strBuilder.append("SCORE: ").append(gc.getHero().getScoreView());
+        font32.draw(batch, strBuilder, 20, 700);
+        strBuilder.clear();
+        strBuilder.append("HEALTH: ").append(gc.getHero().getHealth());
+        font32.draw(batch, strBuilder, 550, 700);
         batch.end();
     }
 }
