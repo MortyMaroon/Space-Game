320c472b5caa419238a71486ada288bd8632e9ea
MortyMaroon <denistkhnv@gmail.com>
2019-12-02 02:01:49 +0300

    Переходит на Game over экран при 0 хп или отсутсвии активных астероидов на экране.

--- GameController.java
+++ GameController.java
@@ -1,19 +1,18 @@
 package com.star.app.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 import com.star.app.screen.ScreenManager;
 
 public class GameController {
     private Background background;
     private AsteroidController asteroidController;
     private BulletController bulletController;
-    private Hero hero;
-    private DropController dropController;
     private ParticleController particleController;
-
-    public ParticleController getParticleController() { return particleController; }
-
-    public DropController getDropController() { return dropController; }
+    private PowerUpsController powerUpsController;
+    private Hero hero;
+    private Vector2 tmpVec;
 
     public AsteroidController getAsteroidController() {
         return asteroidController;
@@ -27,72 +26,110 @@ public class GameController {
         return background;
     }
 
+    public PowerUpsController getPowerUpsController() {
+        return powerUpsController;
+    }
+
+    public ParticleController getParticleController() {
+        return particleController;
+    }
+
     public Hero getHero() {
         return hero;
     }
 
     public GameController() {
-        this.particleController = new ParticleController();
-        this.dropController = new DropController(this);
         this.background = new Background(this);
-        this.hero = new Hero(this);
+        this.hero = new Hero(this, "PLAYER1");
         this.asteroidController = new AsteroidController(this);
-        this.bulletController = new BulletController();
-        for (int i = 0; i < 3; i++) {
+        this.bulletController = new BulletController(this);
+        this.particleController = new ParticleController();
+        this.powerUpsController = new PowerUpsController(this);
+        this.tmpVec = new Vector2(0.0f, 0.0f);
+        for (int i = 0; i < 2; i++) {
             this.asteroidController.setup(MathUtils.random(0, ScreenManager.SCREEN_WIDTH), MathUtils.random(0, ScreenManager.SCREEN_HEIGHT),
                     MathUtils.random(-150.0f, 150.0f), MathUtils.random(-150.0f, 150.0f), 1.0f);
         }
     }
 
     public void update(float dt) {
-        dropController.update(dt);
-        particleController.update(dt);
         background.update(dt);
         hero.update(dt);
         asteroidController.update(dt);
         bulletController.update(dt);
+        particleController.update(dt);
+        powerUpsController.update(dt);
         checkCollisions();
+        checkGameOver();
+    }
+
+    public void checkGameOver() {
+        if (hero.getHp() <= 0 || asteroidController.getActiveList().size() < 1 ){
+            ScreenManager.getInstance().changeScreen(ScreenManager.ScreenType.GAME_OVER);
+        }
     }
 
     public void checkCollisions() {
+        for (int i = 0; i < asteroidController.getActiveList().size(); i++) {
+            Asteroid a = asteroidController.getActiveList().get(i);
+            if (a.getHitArea().overlaps(hero.getHitArea())) {
+                float dst = a.getPosition().dst(hero.getPosition());
+                float halfOverLen = (a.getHitArea().radius + hero.getHitArea().radius - dst) / 2.0f;
+                tmpVec.set(hero.getPosition()).sub(a.getPosition()).nor();
+                hero.getPosition().mulAdd(tmpVec, halfOverLen);
+                a.getPosition().mulAdd(tmpVec, -halfOverLen);
+
+                float sumScl = hero.getHitArea().radius * 2 + a.getHitArea().radius;
+
+                hero.getVelocity().mulAdd(tmpVec, 400.0f * halfOverLen * a.getHitArea().radius / sumScl);
+                a.getVelocity().mulAdd(tmpVec, 400.0f * -halfOverLen * hero.getHitArea().radius / sumScl);
+
+                if (a.takeDamage(2)) {
+                    hero.addScore(a.getHpMax() * 10);
+                }
+                hero.takeDamage(2);
+            }
+        }
+
         for (int i = 0; i < bulletController.getActiveList().size(); i++) {
             Bullet b = bulletController.getActiveList().get(i);
             for (int j = 0; j < asteroidController.getActiveList().size(); j++) {
                 Asteroid a = asteroidController.getActiveList().get(j);
+
                 if (a.getHitArea().contains(b.getPosition())) {
+
+                    particleController.setup(
+                            b.getPosition().x + MathUtils.random(-4, 4), b.getPosition().y + MathUtils.random(-4, 4),
+                            b.getVelocity().x * -0.3f + MathUtils.random(-30, 30), b.getVelocity().y * -0.3f + MathUtils.random(-30, 30),
+                            0.2f,
+                            2.2f, 1.7f,
+                            1.0f, 1.0f, 1.0f, 1.0f,
+                            0.0f, 0.0f, 1.0f, 0.0f
+                    );
+
                     b.deactivate();
                     if (a.takeDamage(1)) {
                         hero.addScore(a.getHpMax() * 100);
+                        for (int k = 0; k < 3; k++) {
+                            powerUpsController.setup(a.getPosition().x, a.getPosition().y, a.getScale() / 4.0f);
+                        }
                     }
                     break;
                 }
             }
         }
 
-        for (int i = 0; i < asteroidController.getActiveList().size(); i++) {
-            Asteroid asteroid = asteroidController.getActiveList().get(i);
-            if (asteroid.getHitArea().contains(hero.getPosition())) {
-                asteroid.takeDamage(1);
-                hero.takeDamage(10);
+        for (int i = 0; i < powerUpsController.getActiveList().size(); i++) {
+            PowerUp p = powerUpsController.getActiveList().get(i);
+            if (hero.getHitArea().contains(p.getPosition())) {
+                hero.consume(p);
+                particleController.getEffectBuilder().takePowerUpEffect(p.getPosition().x, p.getPosition().y);
+                p.deactivate();
             }
         }
+    }
 
-        for (int i = 0; i < dropController.getActiveList().size(); i++) {
-            Drop drop = dropController.getActiveList().get(i);
-            if (drop.getHitArea().contains(hero.getHitArea())){
-                drop.deactivate();
-                switch (MathUtils.random(0,3)){
-                    case 1:
-                        hero.takeMoney(drop.getMoney());
-                        break;
-                    case 2:
-                        hero.takeBullet(drop.getBullet());
-                        break;
-                    case 3:
-                        hero.takeHealth(drop.getHealth());
-                        break;
-                }
-            }
-        }
+    public void dispose() {
+        background.dispose();
     }
 }
