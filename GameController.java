2253cb9b06ffd5fd1e8a1df42329c51dd1bb7fef
MortyMaroon <denistkhnv@gmail.com>
2019-12-05 01:10:58 +0300

    Дроп при попадании в магнитное поле корабля начинается к нему притягиваться. При уничтожении всех астероидов на экране появляются новые с удвоеной силой и здоровьем.

--- GameController.java
+++ GameController.java
@@ -1,11 +1,17 @@
 package com.star.app.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.star.app.screen.ScreenManager;
 
+import static java.lang.Math.*;
+
 public class GameController {
+    private int level;
     private Background background;
     private AsteroidController asteroidController;
     private BulletController bulletController;
@@ -13,6 +19,11 @@ public class GameController {
     private PowerUpsController powerUpsController;
     private Hero hero;
     private Vector2 tmpVec;
+    private Stage stage;
+
+    public Stage getStage() {
+        return stage;
+    }
 
     public AsteroidController getAsteroidController() {
         return asteroidController;
@@ -38,7 +49,7 @@ public class GameController {
         return hero;
     }
 
-    public GameController() {
+    public GameController(SpriteBatch batch) {
         this.background = new Background(this);
         this.hero = new Hero(this, "PLAYER1");
         this.asteroidController = new AsteroidController(this);
@@ -46,9 +57,13 @@ public class GameController {
         this.particleController = new ParticleController();
         this.powerUpsController = new PowerUpsController(this);
         this.tmpVec = new Vector2(0.0f, 0.0f);
+        this.stage = new Stage(ScreenManager.getInstance().getViewport(), batch);
+        this.stage.addActor(hero.getShop());
+        this.level = 1;
+        Gdx.input.setInputProcessor(stage);
         for (int i = 0; i < 2; i++) {
             this.asteroidController.setup(MathUtils.random(0, ScreenManager.SCREEN_WIDTH), MathUtils.random(0, ScreenManager.SCREEN_HEIGHT),
-                    MathUtils.random(-150.0f, 150.0f), MathUtils.random(-150.0f, 150.0f), 1.0f);
+                    MathUtils.random(-150.0f, 150.0f), MathUtils.random(-150.0f, 150.0f), 1.0f, 1, 1);
         }
     }
 
@@ -60,13 +75,45 @@ public class GameController {
         particleController.update(dt);
         powerUpsController.update(dt);
         checkCollisions();
-        checkGameOver();
+        if(!hero.isAlive()) {
+            ScreenManager.getInstance().changeScreen(ScreenManager.ScreenType.GAMEOVER, hero);
+        }
+        if (asteroidController.getActiveList().size() == 0){
+            for (int i = 0; i < 2; i++) {
+                creatingUpgradeAsteroid();
+            }
+        }
+        stage.act(dt);
     }
 
-    public void checkGameOver() {
-        if (hero.getHp() <= 0 || asteroidController.getActiveList().size() < 1 ){
-            ScreenManager.getInstance().changeScreen(ScreenManager.ScreenType.GAME_OVER);
-        }
+    public void creatingUpgradeAsteroid(){
+        asteroidController.setup(MathUtils.random(0, ScreenManager.SCREEN_WIDTH), MathUtils.random(0, ScreenManager.SCREEN_HEIGHT),
+                MathUtils.random(-150.0f, 150.0f), MathUtils.random(-150.0f, 150.0f), 1.0f, 2, 2);
+    }
+
+    public void hit(Hero h, Asteroid a) {
+        // h - 1
+        // a - 2
+        float v1 = h.getVelocity().len();
+        float v2 = a.getVelocity().len();
+
+        float m1 = 0.1f;
+        float m2 = a.getScale();
+
+        float th1 = h.getVelocity().angleRad();
+        float th2 = a.getVelocity().angleRad();
+
+        float phi1 = tmpVec.set(a.getPosition()).sub(h.getPosition()).angleRad();
+        float phi2 = tmpVec.set(h.getPosition()).sub(a.getPosition()).angleRad();
+
+        float v1xN = (float) (((v1 * cos(th1 - phi1) * (m1 - m2) + 2 * m2 * v2 * cos(th2 - phi1)) / (m1 + m2)) * cos(phi1) + v1 * sin(th1 - phi1) * cos(phi1 + PI / 2.0f));
+        float v1yN = (float) (((v1 * cos(th1 - phi1) * (m1 - m2) + 2 * m2 * v2 * cos(th2 - phi1)) / (m1 + m2)) * sin(phi1) + v1 * sin(th1 - phi1) * sin(phi1 + PI / 2.0f));
+
+        float v2xN = (float) (((v2 * cos(th2 - phi2) * (m2 - m1) + 2 * m1 * v1 * cos(th1 - phi2)) / (m2 + m1)) * cos(phi2) + v2 * sin(th2 - phi2) * cos(phi2 + PI / 2.0f));
+        float v2yN = (float) (((v2 * cos(th2 - phi2) * (m2 - m1) + 2 * m1 * v1 * cos(th1 - phi2)) / (m2 + m1)) * sin(phi2) + v2 * sin(th2 - phi2) * sin(phi2 + PI / 2.0f));
+
+        h.getVelocity().set(v1xN, v1yN);
+        a.getVelocity().set(v2xN, v2yN);
     }
 
     public void checkCollisions() {
@@ -78,16 +125,11 @@ public class GameController {
                 tmpVec.set(hero.getPosition()).sub(a.getPosition()).nor();
                 hero.getPosition().mulAdd(tmpVec, halfOverLen);
                 a.getPosition().mulAdd(tmpVec, -halfOverLen);
-
-                float sumScl = hero.getHitArea().radius * 2 + a.getHitArea().radius;
-
-                hero.getVelocity().mulAdd(tmpVec, 400.0f * halfOverLen * a.getHitArea().radius / sumScl);
-                a.getVelocity().mulAdd(tmpVec, 400.0f * -halfOverLen * hero.getHitArea().radius / sumScl);
-
+                hit(hero, a);
                 if (a.takeDamage(2)) {
                     hero.addScore(a.getHpMax() * 10);
                 }
-                hero.takeDamage(2);
+                hero.takeDamage(a.getForce());
             }
         }
 
@@ -120,11 +162,14 @@ public class GameController {
         }
 
         for (int i = 0; i < powerUpsController.getActiveList().size(); i++) {
-            PowerUp p = powerUpsController.getActiveList().get(i);
-            if (hero.getHitArea().contains(p.getPosition())) {
-                hero.consume(p);
-                particleController.getEffectBuilder().takePowerUpEffect(p.getPosition().x, p.getPosition().y);
-                p.deactivate();
+            PowerUp powerUp = powerUpsController.getActiveList().get(i);
+            if (hero.getMagnetiсField().contains(powerUp.getPosition())) {
+                powerUp.getVelocity().set(powerUp.getPosition().sub(hero.getPosition()));
+            }
+            if (hero.getHitArea().contains(powerUp.getPosition())){
+                hero.consume(powerUp);
+                particleController.getEffectBuilder().takePowerUpEffect(powerUp.getPosition().x, powerUp.getPosition().y, powerUp.getType().index);
+                powerUp.deactivate();
             }
         }
     }
